cd4022b5107a955dedc42a7075fc57a00945c3dc
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-14 13:26:00 -0400

    projet35

--- projet3/main.tf
+++ projet3/main.tf
@@ -16,4 +16,23 @@ resource "aws_instance" "my_ec2_instance" {
    tags = {
      Name = "terraform test"
  }
+
+resource "aws_security_group" "instance_sg" {
+    name = "terraform-test-sg"
+
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 }
