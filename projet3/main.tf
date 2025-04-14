3da8e9414d5f03c0b6047e7b05900f8cefd24eae
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-14 14:07:09 -0400

    projet37

--- projet3/main.tf
+++ projet3/main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
     region = "us-east-2"
 }
-resource "aws_instance" "my_ec2_instance" {
-   ami = "ami-07c1207a9d40bc3bd"
 
 resource "aws_security_group" "instance_sg" {
     name = "terraform-test-sg"
@@ -21,17 +19,22 @@ resource "aws_security_group" "instance_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
- 
-   instance_type = "t2.micro"
-   user_data = <<-EOF
+
+resource "aws_instance" "my_ec2_instance" {
+    ami = "ami-07c1207a9d40bc3bd"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+	user_data = <<-EOF
 		#!/bin/bash
         sudo apt-get update
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
-		sudo echo "<h1>Hello devopssec</h1>" > /var/www/html/index.html
+		echo "<h1>Hello devopssec</h1>" > /var/www/html/index.html
 	EOF
-   tags = {
-     Name = "terraform test"
- }
+    
+    tags = {
+        Name = "terraform test"
+    }
 }
