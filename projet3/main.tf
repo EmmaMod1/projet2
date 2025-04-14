e4535fdbcacc399220e9f86fb2d117aebc3b5fc6
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-14 13:11:23 -0400

    projet34

--- projet3/main.tf
+++ projet3/main.tf
@@ -5,6 +5,14 @@ resource "aws_instance" "my_ec2_instance" {
    ami = "ami-07c1207a9d40bc3bd"
  
    instance_type = "t2.micro"
+   user_data = <<-EOF
+		#!/bin/bash
+        sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		sudo echo "<h1>Hello devopssec</h1>" > /var/www/html/index.html
+	EOF
    tags = {
      Name = "terraform test"
  }
