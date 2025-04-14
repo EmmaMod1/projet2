d0e100397405a061f28cc24b0b526159cc3d4c9f
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-14 16:23:08 -0400

    projet3-region1

---
+++ projet3/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+    default = "us-east-1"
+}
+
+variable "AWS_AMIS" {
+    type = "map"
+    default = {
+        "us-east-1" = "ami-085925f297f89fce1"
+        "us-east-2" = "ami-07c1207a9d40bc3bd"
+    }
+}
