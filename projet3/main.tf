1c81c351d3ce48d53e8c6a4b10de381cbd8e8710
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-14 11:15:48 -0400

    projet33

--- projet3/main.tf
+++ projet3/main.tf
@@ -2,9 +2,10 @@ provider "aws" {
     region = "us-east-2"
 }
 resource "aws_instance" "my_ec2_instance" {
-    ami = "ami-07c1207a9d40bc3bd"
-}    
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform test"
+   ami = "ami-07c1207a9d40bc3bd"
+ 
+   instance_type = "t2.micro"
+   tags = {
+     Name = "terraform test"
+ }
 }
