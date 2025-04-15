c4fe3bbaf25fabd0c1084303cccd2551d99521fd
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-15 10:06:37 -0400

    projet333

--- projet3/main.tf
+++ projet3/main.tf
@@ -6,8 +6,9 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-06ffade19910cbfc0"
   instance_type = "t2.micro"
-}
-
+  
   provisioner "local-exec" {
     command = "echo ${aws_instance.my_ec2.public_ip} > ip_address.txt"
+  }
 }
+
