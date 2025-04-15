cbc30fb13517c3a16cbe72384be5b613ebb0472e
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-15 15:41:21 -0400

    mainprincipale1

--- projet3/main.tf
+++ projet3/main.tf
@@ -4,5 +4,5 @@ provider "aws" {
 
 module "website_s3_bucket" {
     source = "./modules/aws-s3-static-website-bucket"
-    bucket_name = "devopssec-terraform"
+    bucket_name = "devopssec-terraform-BiguyB"
 }
