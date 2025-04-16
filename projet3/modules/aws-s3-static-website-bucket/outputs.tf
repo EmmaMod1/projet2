99791aa29cdec244d46adb75dd73001c069a98f2
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-16 10:33:49 -0400

    correction  boucle

--- projet3/modules/aws-s3-static-website-bucket/outputs.tf
+++ projet3/modules/aws-s3-static-website-bucket/outputs.tf
@@ -1,8 +1,3 @@
-output "website_endpoint" {
-    description = "Domain name of the bucket"
-    value       = aws_s3_bucket.s3_bucket.website_endpoint
-}
-
 output "name" {
     description = "Name (id) of the bucket"
     value       = aws_s3_bucket.s3_bucket.id
