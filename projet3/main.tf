22fcf94df323fec66684ef02a9ac6e1ef3e12122
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-16 10:21:10 -0400

    boucle1

--- projet3/main.tf
+++ projet3/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2" 
 }
 
-module "website_s3_bucket" {
-    source = "./modules/aws-s3-static-website-bucket"
-    bucket_name = "devopssec-terraform-biguyb"
+resource "aws_iam_user" "team_iam_user" { 
+    count = 3 
+    name = "user-${count.index}" 
 }
