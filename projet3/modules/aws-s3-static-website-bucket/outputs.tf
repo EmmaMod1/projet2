fb2ba627a640f432b386c971856cf2f581cfee0b
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-16 10:42:56 -0400

    output.tf3

--- projet3/modules/aws-s3-static-website-bucket/outputs.tf
+++ projet3/modules/aws-s3-static-website-bucket/outputs.tf
@@ -1,4 +1,9 @@
-output "name" {
-    description = "Name (id) of the bucket"
-    value       = aws_s3_bucket.s3_bucket.id
+output "iam_user_names" {
+  description = "List of IAM user names"
+  value       = [for user in aws_iam_user.team_iam_user : user.name]
+}
+
+output "iam_user_arns" {
+  description = "List of IAM user ARNs"
+  value       = [for user in aws_iam_user.team_iam_user : user.arn]
 }
