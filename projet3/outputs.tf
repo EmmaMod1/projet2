21054549ff94d94ad5506baec6b51c28e6cc420e
comptesazaz@gmail.com <comptesazaz@gmail.com>
2025-04-16 11:27:42 -0400

    output5

--- projet3/outputs.tf
+++ projet3/outputs.tf
@@ -2,4 +2,3 @@ output "iam_user_descriptions" {
   description = "Liste des utilisateurs IAM avec leur description"
   value       = [for name, role in var.IAM_USERS : "${name} est ${role}"]
 }
-out
